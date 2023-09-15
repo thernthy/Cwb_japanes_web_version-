3b2fbaf335a740b95337849dcb400fb3a3597de2
thernthy <thernthy2003@gmail.com>
2023-09-16 00:01:01 +0700

    update conatact pages

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -1,11 +1,42 @@
 @extends('front.layout')
 
 @push('styles')
+<script src="https://kit.fontawesome.com/c49fa14979.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin=""/>
 @endpush
-
+<style>
+    form{
+        width: 90%;
+    }
+    .form-group input{
+        box-shadow: inset 2px 2px 10px rgba(0, 0, 0, 0.20);
+        border-radius: 30px;
+        background-color: transparent;
+        border: 2px solid #2BFBE8;
+    }
+    .form-group input::placeholder{
+       color: gray;
+    }
+    .form-group textarea{
+        box-shadow: inset 2px 2px 5px rgba(0, 0, 0, 0.30);
+        border-radius: 30px;
+        background-color: transparent;
+        border: 2px solid #2BFBE8;
+    }
+    .contact_icon{
+        margin: auto auto;
+        font-size: 5rem;
+        display: flex;
+        justify-content: flex-end;
+        padding: 10px 0;
+        color: #2BFBE8;
+    }
+    .alert-info{
+        background-color: red;
+    }
+</style>
 @section('content')
-<div class="col pl-4 pr-4">
+<div class="col contact_contaner">
    <!-- <div class="row mt-5">
         <div class="col-xs-12">
             <nav aria-label="breadcrumb">
@@ -16,37 +47,40 @@
             </nav>
         </div>
     </div>-->
-    <div class="row mt-5 pt-5">
-        <div class="col-xs-12 text-tengah">
+    <div class="row mt-5 pt-3 pd-5">
+        <div class="col-6">
             <h1 class="text-xs-center">
                 <span>
                 Contact us
                 </span>
             </h1>
         </div>
-        <div class="offset-md-3 col-md-6 mb-4">
-            <div class="alert alert-success" role="alert">
+        <div class="col-6">
+        <!--    <div class="alert alert-success" role="alert">
                 <h4 class="alert-heading">Submit Form</h4>
                 <p>This form is used to register/applicate/contact regarding our activities and information on this website.</p>
-                <!-- <hr>
-                <p class="mb-0">CWB Community is not </p> -->
-            </div>
+                <hr>
+                <p class="mb-0">CWB Community is not </p>
+            </div>-->
             <form action=''>
+                <div class="contact_icon">
+                <i class="fa-solid fa-comment-dots"></i>
+                </div>
                 <div class="form-group">
-                    <label for="inputAddress">Name</label>
+                    <!-- <label for="inputAddress">Name</label> -->
                     <input type="text" class="form-control" name="name" id="name" placeholder="Your Name">
                 </div>
                 <div class="form-group">
-                    <label for="inputAddress">Address/Origin</label>
+                    <!-- <label for="inputAddress">Address/Origin</label> -->
                     <input type="text" class="form-control" name="address" id="address" placeholder="Your Address/Origin">
                 </div>
                 <div class="form-group">
-                    <label for="inputAddress2">e-mail</label>
+                    <!-- <label for="inputAddress2">e-mail</label> -->
                     <input type="email" class="form-control" name="email" id="email"
                         placeholder="your@emailaddress">
                 </div>
                 <div class="form-group">
-                    <label for="inputAddress2">Message</label>
+                    <!-- <label for="inputAddress2">Message</label> -->
                     <textarea class="form-control" name="" id="" rows="3"></textarea>
                 </div>
                 <div class="alert alert-info" role="alert">
