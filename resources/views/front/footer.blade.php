c6725cbf749755b7da82e22dcac0a22c822ea7b2
thernthy <thernthy2003@gmail.com>
2024-01-10 16:32:02 +0700

    last update desing for home page

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,12 +1,42 @@
-<footer>
+<footer style="background-color: #774332; padding:10px 0;">
     <div class="container">
-
             <div class="row">
-                <div class="logo">
-                    <b>W</b>
+                <div class="col-sm-3 company_slugan">
+                    <div class="logo">
+                        <b>W</b>
+                    </div>
+                    <p class="text-center">
+                    国境なく働くということは、世界中の人々と協力することを意味するかもしれません。
+                    </p>
                 </div>
-                <p>
-                国境なく働くということは、世界中の人々と協力することを意味するかもしれません。
+                <div class="col-sm-3 company_slugan">
+                </div>
+                <div class="col-sm-3 company_slugan">
+                    <div id="ignielSubscribe">
+                        <div class="email__">
+                            <div class="email">
+                              <h3 style="color: white;"> 購読する</h3>
+                                <form action="https://feedburner.google.com/fb/a/mailverify?uri=igniel" method="post" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=igniel', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true" target="popupwindow">
+                                    <input name="email" class="email" placeholder="Contoh: nama@mail.com"/>
+                                    <input name="uri" type="hidden" value="igniel" /> <input name="loc" type="hidden" value="en_US" />
+                                    <button type="submit"></button>
+                                </form>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            <div class="row pb-2">
+                <div class="medsos__">
+                    <div class="medsos">
+                    <a class="facebook" title="Facebook" href="https://www.facebook.com/xxxx" rel="nofollow noopener" target="_blank"><svg viewbox="0 0 24 24"> <path d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z"></path></svg></a>
+                    <a class="youtube" title="Youtube" href="https://www.youtube.com/xxxx" rel="nofollow noopener" target="_blank"><svg viewbox="0 0 24 24"><path d="M10,16.5V7.5L16,12M20,4.4C19.4,4.2 15.7,4 12,4C8.3,4 4.6,4.19 4,4.38C2.44,4.9 2,8.4 2,12C2,15.59 2.44,19.1 4,19.61C4.6,19.81 8.3,20 12,20C15.7,20 19.4,19.81 20,19.61C21.56,19.1 22,15.59 22,12C22,8.4 21.56,4.91 20,4.4Z"> </path></svg></a>
+                    <a class="instagram" title="Instagram" href="https://www.instagram.com/xxxx" rel="nofollow noopener" target="_blank"><svg viewbox="0 0 24 24"><path d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z"></path></svg></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <p class="text-center">
+                    2024@phumasiaActivity_japanes_v
                 </p>
             </div>
     </div>
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,11 +3,12 @@
 @push('styles')
 <style>
     .side_bar_right{
-        background: #b7a7a7;
+        background: #EAE5E3;
         padding-bottom: 20px;
     }
     .side_bar_right h2{
         padding:10px 0;
+        color: #7E3F3F;
         border-bottom: 1.5px solid #978080;
     }
     .side_bar_right li{
@@ -19,13 +20,84 @@
         text-decoration: none;
     }
     .side_bar_right .realative_page{
-        padding: 5px 5px;
+        width: 100%;
+        padding: 8px 0;
         background: #9D5139;
-        border-radius: 10px;
+        border-radius: 27px;
+        transform: rotate(4deg);
+        padding-left: 10px;
+        box-shadow: inset 1px 1px 10px rgb(38 37 37 / 84%);
+        margin-bottom: 2rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .side_bar_right .realative_page a{
         color: white;
         text-decoration: none;
+         margin:0 10%;
+         z-index: 100;
     }
-
+    .realative_page i{
+        padding: 10px;
+        color: white;
+        background: #978080;
+        border-radius: 50%;
+        box-shadow: 1px 1px 10px rgb(38 37 37 / 84%);
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translateY(-50%);
+        z-index: 50;
+    }
+    .realative_page:hover >i{
+        animation: moveRight .5s forwards;
+    }
+    .img_galery{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .img_galery .img_blocks{
+        width: 33.33%;
+        height: 150px;
+        border-radius: 10px;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        transform: rotate(11deg) translateX(10px);
+        z-index: 10;
+        transition: all .5s;
+    }
+    .img_blocks:hover {
+        z-index: 15;
+        transform: rotate(0);
+    }
+    .img_blocks:nth-child(2):hover {
+        transform: rotate(0deg) translateY(-20px);
+        z-index: 15;
+    }
+    .img_blocks:last-child:hover {
+        transform: rotate(0deg) translateX(-10px)  translateY(-30px);
+        z-index: 15;
+    }
+    .img_blocks:nth-child(2){
+        transform: rotate(11deg) translateY(-20px);
+        z-index: 5;
+    }
+    .img_blocks:last-child{
+        transform: rotate(11deg) translateX(-10px) translateY(-30px);
+        z-index: 2;
+    }
+    @keyframes moveRight {
+            0% {
+                left: 0;
+                background: #978080;
+            }
+            100% {
+                left: 91%; /* Adjust this value based on how far you want the element to move */
+                background: green;
+            }
+        }
 </style>
 @endpush
 
@@ -34,7 +106,7 @@
 		<source src="{{ asset('img/video/Home_page_cover.mp4') }}" type="video/mp4">
     </video>-->
     <div class="container-fluid">
-        <div class="row pt-5">
+        <div class="row">
             <div class="col-sm-8">
                 
             </div>
@@ -57,8 +129,26 @@
                         <a href="">教育</a>
                     </li>
                 </ul>
-                <div style="transform: rotate(4deg);">
-                    <a href="" class="realative_page">ミャンマーのページとの比較</a>
+                <div class="realative_page mt-5">
+                    <i class="fa-solid fa-arrow-right"></i>
+                    <a href="#sfs" >ミャンマーのページとの比較 </a>
+                </div>
+                <hr>
+                <h2>当社の魅力</h2>
+                <p class="mt-3 mb-5">
+                    CWBのミッション
+                    国境なく働くということは、地理的な境界に制約されずに世界中の人々と協力することを意味するかもしれません。
+                    これは多くの場合、テクノロジーとリモートワークの取り決めによって促進されます。
+                </p>
+                <div class="img_galery">
+                    <div class="img_blocks" style="background-image: url('{{asset('img/dace2.jpg') }}');">
+                      
+                    </div>
+                    <div class="img_blocks" style="background-image: url('{{asset('img/dace2.jpg') }}');">
+
+                    </div>
+                    <div class="img_blocks" style="background-image: url('{{asset('img/dace2.jpg') }}');">
+                    </div>
                 </div>
             </div>
         </div>
